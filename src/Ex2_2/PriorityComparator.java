bb98f1a4e479abd93011e1ba5a00734e07d326ff
shira-chesler <shira.chesler@gmail.com>
2023-01-09 14:40:30 +0200

    updated comparator - only by priority

--- src/Ex2_2/PriorityComparator.java
+++ src/Ex2_2/PriorityComparator.java
@@ -6,18 +6,8 @@ public class PriorityComparator implements Comparator<Task> {
 
     @Override
     public int compare(Task o1, Task o2) {
-        TaskType o1_type = o1.getTaskType();
-        TaskType o2_type = o2.getTaskType();
-        if(!(o1_type == o2_type)){
-            if (o2_type.compareTo(o1_type)>0){
-                return -1;
-            }
-            else {
-                return 1;
-            }
-        }
-        int o1_prior = o1_type.getPriorityValue();
-        int o2_prior = o2_type.getPriorityValue();
+        int o1_prior = o1.getTaskType().getPriorityValue();
+        int o2_prior = o2.getTaskType().getPriorityValue();
         if (o1_prior < o2_prior){//means o1 priority is higher
             return 1;
         }
